eee403b9ba97173b4b4be02366cb209a391cb661
Pranav <pranavva34@gmail.com>
2022-02-07 15:57:46 +0530

    added d3 functionality

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,6 +10,9 @@ exports.createPages = async ({ graphql, actions }) => {
           title
           _rawSlug
           _id
+          categories {
+            title
+          }
         }
       }
     }
@@ -19,12 +22,16 @@ exports.createPages = async ({ graphql, actions }) => {
     throw result.errors;
   }
 
-  const pageTemplate = path.resolve("./src/templates/post.js");
+  //const pageTemplate = path.resolve("./src/templates/post.js");
 
   const posts = result.data.allSanityPost.nodes || [];
   posts.forEach((post, index) => {
+    console.log(post);
     const Path = `/published/${post._rawSlug.current}`;
-
+    let pageTemplate =
+      post.categories[0].title == "d3"
+        ? path.resolve(`./src/pages/${post._rawSlug.current}.js`)
+        : path.resolve("./src/templates/post.js");
     createPage({
       path: Path,
       component: pageTemplate,
